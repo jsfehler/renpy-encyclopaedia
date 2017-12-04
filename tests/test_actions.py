c05d87d99be0570412e99070298b4adad94563a7
Joshua Fehler <jsfehler@gmail.com>
2017-12-03 20:19:25 -0500

    Tests: Add ChangeEntry and ChangePage tests

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,7 +2,77 @@ from encyclopaedia.encyclopaedia import Encyclopaedia
 from encyclopaedia.encentry import EncEntry
 
 
-def test_next_entry():
+def test_reset_sub_page():
+    enc = Encyclopaedia()
+
+    e = EncEntry(
+        parent=enc,
+        name="Test Name",
+        text=["Test Text"],
+    )
+
+    ee = EncEntry(
+        parent=e,
+        name="Sub1",
+        text=["Test Text"],
+    )
+
+    eee = EncEntry(
+        parent=e,
+        name="Sub2",
+        text=["Test Text"],
+    )
+
+    enc.SetEntry(e)()
+
+    assert e == enc.active
+    assert enc.sub_current_position == 1
+
+    enc.NextPage()()
+    enc.NextPage()()
+
+    assert enc.sub_current_position == 3
+    assert e.current_page == eee
+
+    enc.ResetSubPage()()
+
+    assert enc.sub_current_position == 1
+    assert e.current_page == e
+
+
+def test_next_page():
+    enc = Encyclopaedia()
+
+    e = EncEntry(
+        parent=enc,
+        name="Test Name",
+        text=["Test Text"],
+    )
+
+    ee = EncEntry(
+        parent=e,
+        name="Sub1",
+        text=["Test Text"],
+    )
+
+    eee = EncEntry(
+        parent=e,
+        name="Sub2",
+        text=["Test Text"],
+    )
+
+    enc.SetEntry(e)()
+
+    assert e == enc.active
+    assert enc.sub_current_position == 1
+
+    enc.NextPage()()
+
+    assert enc.sub_current_position == 2
+    assert e.current_page == ee
+
+
+def test_previous_entry():
     """Test Actions through their implementation in Encyclopaedia."""
 
     enc = Encyclopaedia()
@@ -15,15 +85,23 @@ def test_next_entry():
             locked=False
         )
 
-    for x in range(5, 10):
-        EncEntry(
-            parent=enc,
-            name="Test Name {}".format(x + 1),
-            text=["Test Text"],
-            locked=True
-        )
+    assert 0 == enc.current_position
 
-    for x in range(10, 15):
+    enc.NextEntry()()
+
+    enc.PreviousEntry()()
+
+    assert enc.current_entries[0] == enc.active
+    assert enc.current_entries[0].viewed
+    assert 0 == enc.current_position
+
+
+def test_next_entry():
+    """Test Actions through their implementation in Encyclopaedia."""
+
+    enc = Encyclopaedia()
+
+    for x in range(0, 5):
         EncEntry(
             parent=enc,
             name="Test Name {}".format(x + 1),

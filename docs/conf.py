3cca474a796a405ffc43de925daa2a20a4c1e0ca
Joshua Fehler <jsfehler@gmail.com>
2023-04-23 20:02:50 -0400

    docs: Add missing extension

--- docs/conf.py
+++ docs/conf.py
@@ -37,13 +37,16 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon']
+    'sphinx.ext.napoleon',
+    'sphinx_immaterial',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

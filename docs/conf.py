93939ca75f0f26b3a42c3a852e2adc01db66fbc1
Joshua Fehler <jsfehler@gmail.com>
2023-04-26 21:03:13 -0400

    docs: Remove unused theme option

--- docs/conf.py
+++ docs/conf.py
@@ -149,11 +149,6 @@ html_theme_options = {
 
 }
 
-html_theme_options['extra_nav_links'] = {
-    "Documentation": "documentation.html",
-    "Module Index": "py-modindex.html"
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

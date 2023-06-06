3254df9622ea450cba7cf08f9c2ec41429ef5723
Joshua Fehler <jsfehler@gmail.com>
2023-06-06 08:59:48 -0400

    Change EventEmitter.on() decorator return type

--- encyclopaedia/eventemitter_ren.py
+++ encyclopaedia/eventemitter_ren.py
@@ -11,11 +11,14 @@ class EventEmitter:
     def __init__(self) -> None:
         self.callbacks: dict[str, list[CALLBACK_TYPE]] = {}
 
-    def on(self, ev_name: str) -> Callable[[CALLBACK_TYPE], None]:
+    def on(self, ev_name: str) -> Callable[[CALLBACK_TYPE], CALLBACK_TYPE]:
         """Decorator to register a new callback function to an event."""
-        def wrapper(callback: CALLBACK_TYPE) -> None:
+
+        def wrapper(callback: CALLBACK_TYPE) -> CALLBACK_TYPE:
             self.callbacks[ev_name] = self.callbacks.get(ev_name, [])
             self.callbacks[ev_name].append(callback)
+
+            return callback
 
         return wrapper
 

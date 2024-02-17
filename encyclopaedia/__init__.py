cb5f5d7450bd50a9f372a61f325e42f3d3ac1def
Joshua Fehler <jsfehler@gmail.com>
2024-02-17 14:45:49 -0500

    Project version is now centralized inside encyclopaedia/version.py

--- encyclopaedia/__init__.py
+++ encyclopaedia/__init__.py
@@ -6,6 +6,7 @@ Ren'Py has no use for it.
 from encyclopaedia.encentry_ren import EncEntry
 from encyclopaedia.encyclopaedia_ren import Encyclopaedia
 from encyclopaedia.encentry_template_ren import EncEntryTemplate
+from encyclopaedia.version import __version__  # noqa: F401
 
 
 __all__ = [

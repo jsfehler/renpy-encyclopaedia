d62d112abd9df9768d31b1a40a91dab77b2a9d16
Joshua Fehler <jsfehler@gmail.com>
2024-10-20 17:58:30 -0400

    fix: Remove unused assignment

--- tests/book/test_book_set_active_page.py
+++ tests/book/test_book_set_active_page.py
@@ -42,7 +42,7 @@ def test_book_current_page_alias():
     book = Book(title="Greek Gods", subject="Mythology")
 
     EncEntry(parent=book, number=0, name="Zeus", text="Dummy Text")
-    hades = EncEntry(parent=book, number=1, name="Hades", text="Dummy Text")
+    EncEntry(parent=book, number=1, name="Hades", text="Dummy Text")
 
     book.set_active_page(1)
     assert book.current_page == book.active

2c4232b9ed0a446ba398bd5c85325753f19cddbc
Joshua Fehler <jsfehler@gmail.com>
2024-10-20 17:55:18 -0400

    tests: Add missing unit test

--- tests/book/test_book_set_active_page.py
+++ tests/book/test_book_set_active_page.py
@@ -36,3 +36,13 @@ def test_book_set_active_page_invalid_over_max():
         book.set_active_page(100)
 
     assert book.active == zeus
+
+
+def test_book_current_page_alias():
+    book = Book(title="Greek Gods", subject="Mythology")
+
+    EncEntry(parent=book, number=0, name="Zeus", text="Dummy Text")
+    hades = EncEntry(parent=book, number=1, name="Hades", text="Dummy Text")
+
+    book.set_active_page(1)
+    assert book.current_page == book.active

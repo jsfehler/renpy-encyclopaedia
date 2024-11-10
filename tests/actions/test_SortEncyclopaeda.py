795036df9afedfef5971797c610fc53367d98fee
Joshua Fehler <jsfehler@gmail.com>
2024-11-10 12:16:46 -0500

    tests: Add SortEncyclopaedia tests

---
+++ tests/actions/test_SortEncyclopaeda.py
@@ -0,0 +1,62 @@
+import pytest
+
+from encyclopaedia import EncEntry, Encyclopaedia, constants_ren
+
+
+@pytest.fixture()
+def sortable_enc():
+    """Create an encyclopaedia for filtering tests."""
+    enc = Encyclopaedia()
+
+    EncEntry(
+        parent=enc,
+        name="Apple",
+        text=[""],
+        subject="Fruit",
+    )
+
+    EncEntry(
+        parent=enc,
+        name="Banana",
+        text=[""],
+        subject="Fruit",
+    )
+
+    EncEntry(
+        parent=enc,
+        name="Grape",
+        text=[""],
+        subject="Fruit",
+    )
+
+    return enc
+
+
+def test_sort_encyclopaedia_reverse_alphabetical(sortable_enc):
+    """Test Actions through their implementation in Encyclopaedia."""
+    action = sortable_enc.Sort(constants_ren.SortMode.REVERSE_ALPHABETICAL)
+
+    assert sortable_enc.current_entries[0].name == "Apple"
+
+    action()
+
+    assert sortable_enc.current_entries[0].name == "Grape"
+
+
+def test_sort_encyclopaedia_get_selected(sortable_enc):
+    """Test Actions through their implementation in Encyclopaedia."""
+    action = sortable_enc.Sort(constants_ren.SortMode.SUBJECT)
+    action()
+
+    result = action.get_selected()
+
+    assert result is True
+
+
+def test_sort_encyclopaedia_get_selected_no_filter(sortable_enc):
+    """Test Actions through their implementation in Encyclopaedia."""
+    action = sortable_enc.Sort(constants_ren.SortMode.SUBJECT)
+
+    result = action.get_selected()
+
+    assert result is False
